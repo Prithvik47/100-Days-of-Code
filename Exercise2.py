6b4c449f2e05f71fbe2f48ad577166514f7237fa
Prithvik Hareesh <127434182+Prithvik47@users.noreply.github.com>
2023-07-21 22:49:18 +0530

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Genarator")
+
+city = input("Which City did you grow up in:\n")
+
+pet = input("What is your pet's name:\n")
+
+print("your Band Name could be:\n" + city + " " + pet)
+
+
